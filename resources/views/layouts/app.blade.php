5c3c37738477da2338054eeacfbaed4035893ce3
JamaludinHanif <140181873+JamaludinHanif@users.noreply.github.com>
2024-10-09 14:58:34 +0700

    add loading spinner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,15 @@
 
 <body>
 
+    <div id="loading-spinner"
+        style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(255,255,255,0.8); z-index: 9999; text-align: center;">
+        <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    </div>
+
     <div class="wrapper">
         {{-- header --}}
         @include('components.nav-bar')
